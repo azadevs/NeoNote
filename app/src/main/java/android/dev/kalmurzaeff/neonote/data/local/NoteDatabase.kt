7c5854383b21ecda23f7ff9901b8f524d8b11279
azadevs <azamatdeveloper3@gmail.com>
2024-01-30 23:50:33 +0500

    Removed unneeded code and decreased database version

--- app/src/main/java/android/dev/kalmurzaeff/neonote/data/local/NoteDatabase.kt
+++ app/src/main/java/android/dev/kalmurzaeff/neonote/data/local/NoteDatabase.kt
@@ -5,26 +5,9 @@ import android.dev.kalmurzaeff.neonote.data.local.entity.NoteEntity
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [NoteEntity::class], version = 2, exportSchema = false)
+@Database(entities = [NoteEntity::class], version = 1, exportSchema = false)
 abstract class NoteDatabase : RoomDatabase() {
-
 
     abstract fun noteDao(): NoteDao
 
-//    companion object {
-//        @Volatile
-//        var INSTANCE: NoteDatabase? = null
-//
-//        fun getInstance(context: Context): NoteDatabase =
-//            INSTANCE ?: synchronized(this) {
-//                INSTANCE
-//                    ?: buildDatabase(context).also {
-//                        INSTANCE = it
-//                    }
-//            }
-//
-//        private fun buildDatabase(context: Context) =
-//            Room.databaseBuilder(context, NoteDatabase::class.java, "note_database")
-//                .allowMainThreadQueries().build()
-//    }
 }
